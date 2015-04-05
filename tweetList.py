92369388cbb68cb1105b817f23203c3d987177c3
Tortillaguy <agcacho22@gmail.com>
2015-04-04 20:34:10 -0700

    Added sockets

--- tweetList.py
+++ tweetList.py
@@ -3,7 +3,14 @@
 __author__= "Kyle Calica & Adrian Cacho"
 
 import tweepy
+import socket
+import json
+
 from tweepy.streaming import StreamListener
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
 
 consumer_key = "8jbtWe9xKzrM051Tmb0c81ZM2"
 consumer_secret = "SvNfaCbgDUmXb9GCnrmH9zcjl8ti0SeDRs0E20czTbl4dkL3F4" 
@@ -15,28 +22,40 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-print "Trying the Search Method now: \n"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
 
+
+print "Trying the Search Method now: \n"
 q = raw_input("Enter Hashtag: ")
 
-"""
-tweets = api.search(q)
 
-for tweet in tweets:
-     print tweet.user.name + " : " + tweet.text
-"""
 
 #overriding the StreamListener class
-class StdOutListener(StreamListener):
+class StdOutListener(tweepy.StreamListener):
 	
 	def on_status(self, status):
-		print(status.text)
-
+		
+		raw = status.text[4:] # cuts the " u' " before every text
+		text = raw.encode('utf-8')
+		
+		try:
+			media = status.entities['media'][0]['media_url']
+			url = json.dumps(media)
+			media_url = url.strip(' " ')
+		except: 
+			media_url = "No Media"
+		
+		gather = text + " | " + media_url
+		
+		s.send(gather)
+		print gather
+		return True
 # once we have an api object and a status listener we can create our stream object
 
 myStreamListen = StdOutListener()
 myStream = tweepy.Stream(auth = api.auth, listener = myStreamListen)
-myStream.filter(track=[str(q)])
+myStream.filter(track=[str(q)], async=True)
 
 #http://tweepy.readthedocs.org/en/v3.3.0/streaming_how_to.html?highlight=streamming
 #http://adilmoujahid.com/posts/2014/07/twitter-analytics/
